b3bc7e7fca647459da5c18197c7f8d12d3c08f73
Mateus Correia <correia.mateus@hotmail.com>
2024-04-18 01:32:10 -0300

    Adicionado testes de signup dono de buffet

--- spec/system/auth/buffet_owner_sign_up_spec.rb
+++ spec/system/auth/buffet_owner_sign_up_spec.rb
@@ -1,1 +1,19 @@
 require 'rails_helper'
+
+describe 'Dono de buffet cria conta' do
+  it 'com sucesso' do
+    visit root_path
+    click_on 'Entrar'
+    click_on 'Sign up'
+    fill_in 'Name', with: 'Mateus Buffet Owner'
+    fill_in 'Email', with: 'mateus@gmail.com'
+    fill_in 'Password', with: '123456'
+    fill_in 'Password confirmation', with: '123456'
+    click_on 'Sign up'
+
+    expect(page).to have_content 'mateus@gmail.com'
+    expect(page).to have_button 'Sair'
+    user = BuffetOwner.last
+    expect(user.name).to eq 'Mateus Buffet Owner'
+  end
+end
