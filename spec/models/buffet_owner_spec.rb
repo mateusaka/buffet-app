0f2766c3e890e249a6a7543861b1331285e77ac7
Mateus Correia <correia.mateus@hotmail.com>
2024-04-20 10:05:03 -0300

    Adicionado testes model dono de buffet

--- spec/models/buffet_owner_spec.rb
+++ spec/models/buffet_owner_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe BuffetOwner, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#valid?' do
+    it 'falso quando email está vazio' do
+      buffet_owner = BuffetOwner.new(
+        name: 'Mateus Buffet Owner',
+        email: '',
+        password: '123456'
+      )
+
+      result = buffet_owner.valid?
+
+      expect(result).to eq false
+    end
+
+    it 'falso quando password está vazio' do
+      buffet_owner = BuffetOwner.new(
+        name: 'Mateus Buffet Owner',
+        email: 'mateus@gmail.com',
+        password: ''
+      )
+
+      result = buffet_owner.valid?
+
+      expect(result).to eq false
+    end
+  end
 end
